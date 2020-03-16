75375fd0a5872d19fe56be0dbd4892314802069f
Leonardo Figueiredo <leommko@gmail.com>
2020-03-16 16:47:19 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Login Facebook</title>
+  <!-- Aplicar depois meta tags -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+  <link href="https://fonts.googleapis.com/css?family=Lato:400,700&display=swap" rel="stylesheet" />
+  <link href="css/style.css" rel="stylesheet" />
+</head>
+
+<body>
+  <header>
+    <div class="center">
+      <div class="logo">
+        <h2>facebook</h2>
+      </div>
+      <!-- logo -->
+
+      <form method="post" class="form-login">
+        <div class="form-element">
+          <p>E-mail ou telefone:</p>
+          <input type="email" />
+        </div>
+        <div class="form-element">
+          <p>Senha:</p>
+          <input type="password" />
+        </div>
+        <!-- form-element -->
+        <div class="form-element">
+          <input type="submit" name="acao" value="Enviar" />
+        </div>
+      </form>
+      <!-- form-login -->
+      <div class="clear"></div>
+    </div>
+    <!-- center -->
+  </header>
+
+  <section class="main">
+    <div class="center">
+      <div class="img-pessoas">
+        <img src="images/wallpaper.png" alt="img-pessoas" />
+      </div>
+
+      <div class="abrir-conta">
+        <h2>Abra sua conta</h2>
+        <h3>É gratuito e sempre será</h3>
+
+        <form class="criar-conta">
+          <div class="w50">
+            <input type="text" name="Nome" placeholder="Nome" />
+          </div>
+          <!-- w50 -->
+
+          <div class="w50">
+            <input type="text" name="Sobrenome" placeholder="Sobrenome" />
+          </div>
+          <!-- w50 -->
+
+          <div class="w100">
+            <input type="email" name="Celular ou email" placeholder="Celular ou email" />
+          </div>
+          <!-- w100 -->
+
+          <div class="w100">
+            <input type="password" name="Nova senha" placeholder="Nova senha" />
+          </div>
+          <!-- w100 -->
+
+          <div class="w100">
+            <h2>Data de Nascimento:</h2>
+            <select name="nascimento-dia" class="nascimento">
+              <?php
+              for ($i = 1; $i <= 31; $i++) {
+
+              ?>
+                <option value="<?php echo $i; ?>"><?php echo $i ?></option>
+              <?php } ?>
+
+            </select>
+            <select name="nascimento-mes" class="nascimento">
+              <option value="0">Janeiro</option>
+              <option value="1">Fevereiro</option>
+              <option value="2">Março</option>
+              <option value="3">Abril</option>
+              <option value="4">Maio</option>
+              <option value="5">Junho</option>
+              <option value="6">Julho</option>
+              <option value="7">Agosto</option>
+              <option value="8">Setembro</option>
+              <option value="9">Outubro</option>
+              <option value="10">Novembro</option>
+              <option value="11">Dezembro</option>
+            </select>
+            <select name="nascimento-ano" class="nascimento">
+              <?php
+              for ($i = 1900; $i <= 2020; $i++) {
+              ?>
+                <option value="<?php echo $i; ?>"><?php echo $i ?></option>
+              <?php } ?>
+            </select>
+          </div>
+          <!-- w100 -->
+
+          <div class="w100">
+            <div class="input-radio">
+              <input type="radio" name="sexo" value="masculino">
+              <h2>Masculino</h2>
+            </div>
+            <!-- radio -->
+            <div class="input-radio">
+              <input type="radio" name="sexo" value="feminino">
+              <h2>Feminino</h2>
+            </div>
+            <!-- radio -->
+            <div class="clear"></div>
+          </div>
+          <!-- w100 -->
+
+
+          <div class="w100">
+            <input type="submit" name="acao" value="Cadastre-se" />
+          </div>
+          <!-- w100 -->
+
+          <div class="clear"></div>
+        </form>
+        <!-- criar-conta -->
+      </div>
+      <!-- abrir-conta -->
+
+      <div class="clear"></div>
+    </div>
+    <!-- center -->
+  </section>
+  <!-- main-section -->
+
+  <section class="linguas">
+    <div class="center">
+      <a class="selected-lingua" href="#">Português (BR)</a>
+      <a href="#">Português (BR)</a>
+      <a href="#">Português (BR)</a>
+      <a href="#">Português (BR)</a>
+      <a href="#">Português (BR)</a>
+      <a href="#">Português (BR)</a>
+      <a href="#">Português (BR)</a>
+      <a href="#">Português (BR)</a>
+      <a href="#">Português (BR)</a>
+    </div>
+
+    <div style="border:0; padding-top:20px;" class="center">
+      <a href="#">Português (BR)</a>
+      <a href="#">Português (BR)</a>
+      <a href="#">Português (BR)</a>
+      <a href="#">Português (BR)</a>
+      <a href="#">Português (BR)</a>
+      <a href="#">Português (BR)</a>
+      <a href="#">Português (BR)</a>
+      <a href="#">Português (BR)</a>
+      <a href="#">Português (BR)</a>
+    </div>
+
+  </section>
+  <!-- linguas -->
+
+</body>
+
+</html>
